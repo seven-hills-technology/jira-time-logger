049c29930995ec2cdb1f461986c24b18b655dc21
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-10-23 14:37:42 -0400

    added redis host and port to the config

--- src/services/redisJiraService.ts
+++ src/services/redisJiraService.ts
@@ -1,10 +1,16 @@
 import * as redis from "redis";
 import { promisify } from "util";
+import { config } from "../config"
 
 import { RedisJiraIssue } from "../models/redisJiraIssue";
 
+const redisHost = config.redisHost;
+const redisPort = config.redisPort;
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+    host: redisHost,
+    port: redisPort
+});
 const asyncRedisClient = {
     get: promisify(redisClient.get).bind(redisClient),
     set: promisify(redisClient.set).bind(redisClient),
@@ -18,7 +24,6 @@ const asyncRedisClient = {
 
 class RedisJiraService {
     async getAllJiraIssues(): Promise<RedisJiraIssue[]> {
-
         const keys: string[] = await asyncRedisClient.keys("jira-issue-cache:jiraIssues:*");
         const redisJiraIssues: RedisJiraIssue[] = await Promise.all(
             keys.map(key => asyncRedisClient.get(key)
