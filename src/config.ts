c306e58cc212c9490bb5bc9f81b643320a0d1eec
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-10-23 14:37:42 -0400

    added redis host and port to the config

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,7 @@ const jsonConfig = require("electron-json-config");
 export const config = {
     jiraApiBaseUrl: process.env.JIRA_API_BASE_URL || jsonConfig.get("jiraApiBaseUrl"),
     jiraApiAuthHeaderValue: process.env.JIRA_API_AUTH_HEADER_VALUE || jsonConfig.get("jiraApiAuthHeaderValue"),
-    jiraUnclassifiedIssueKey: process.env.JIRA_UNCLASSIFIED_ISSUE_KEY || jsonConfig.get("jiraUnclassifiedIssueKey")
+    jiraUnclassifiedIssueKey: process.env.JIRA_UNCLASSIFIED_ISSUE_KEY || jsonConfig.get("jiraUnclassifiedIssueKey"),
+    redisHost: process.env.REDIS_HOST || jsonConfig.get("redisHost"),
+    redisPort: process.env.REDIS_PORT || jsonConfig.get("redisPort")
 }
